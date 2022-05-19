77b7840cc169ede44783f6680cd94aab0bc00916
Taewoo Kim <embedded61@gmail.com>
2022-05-19 17:16:36 +0900

    Add files via upload

---
+++ 940.js
@@ -0,0 +1,48 @@
+"use strict";
+(self["webpackChunkremote1"] = self["webpackChunkremote1"] || []).push([[940],{
+
+/***/ 940:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var libs_react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(540);
+/* harmony import */ var libs_react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(libs_react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var libs_react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(43);
+/* harmony import */ var libs_react_router_dom__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(libs_react_router_dom__WEBPACK_IMPORTED_MODULE_1__);
+var _s = $RefreshSig$();
+
+
+
+
+const Button = () => {
+  _s();
+
+  let history = (0,libs_react_router_dom__WEBPACK_IMPORTED_MODULE_1__.useHistory)();
+
+  function handleClick() {
+    history.push('/home');
+  }
+
+  return /*#__PURE__*/libs_react__WEBPACK_IMPORTED_MODULE_0___default().createElement("button", {
+    onClick: handleClick
+  }, "from remote1: GO HOME");
+};
+
+_s(Button, "9cZfZ04734qoCGIctmKX7+sX6eU=", false, function () {
+  return [libs_react_router_dom__WEBPACK_IMPORTED_MODULE_1__.useHistory];
+});
+
+_c = Button;
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Button);
+
+var _c;
+
+$RefreshReg$(_c, "Button");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=940.js.map
